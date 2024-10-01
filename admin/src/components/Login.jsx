1b71b797f2df10afdcf7bcaa808e1f6dff4ba3a5
ErayKoybasi <eraykoybasi.34@gmail.com>
2024-10-01 16:28:01 +0300

    second commit

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Login = ({setToken}) => {
